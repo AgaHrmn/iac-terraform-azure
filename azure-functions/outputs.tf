14f6f9131c25f6758d45881578c51ecc2240491a
AgaHrmn <a.olejnikk@gmail.com>
2024-06-25 15:22:15 +0200

    first commit

---
+++ azure-functions/outputs.tf
@@ -0,0 +1,5 @@
+output "function_id" {
+  description = "The ID of the Function App Function"
+  value       = azurerm_function_app_function.function.id
+}
+
---
+++ azure-functions/variables.tf
@@ -0,0 +1,25 @@
+variable "resource_group_name" {
+  type        = string
+}
+
+variable "location" {
+  type        = string
+  default     = "West Europe"
+}
+
+variable "storage_account_name" {
+  type        = string
+}
+
+variable "service_plan_name" {
+  type        = string
+}
+
+variable "function_app_name" {
+  type        = string
+}
+
+variable "function_name" {
+  type        = string
+}
+
